32e2cde322ede4d9395bf2377f2e57b27e6876b2
Dmitry Lyubetsky <dmitridad@orbsoft.ru>
2020-03-11 19:41:38 +0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'token' => '824976833:AAEyEnTNafq86nX2jk0xGJyTEhwqHueBWpw'
+);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+$apiConfig = include_once(__DIR__. '/config/config.php');
+
+echo $apiConfig['token'];
